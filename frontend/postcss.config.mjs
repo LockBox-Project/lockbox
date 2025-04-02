3a3b962541789c3afe963777af315a7279ba0a9f
Guilherme Monteiro <guilherme.dias.monteiro@tecnico.ulisboa.pt>
2025-04-02 15:51:41 +0100

    backend done

--- frontend/postcss.config.mjs
+++ frontend/postcss.config.mjs
@@ -1,5 +1,5 @@
 const config = {
-    plugins: ["@tailwindcss/postcss"], // Instalar autoprefixer
+    plugins: ["@tailwindcss/postcss", "autoprefixer"], // Instalar autoprefixer
   };
   
   export default config;
