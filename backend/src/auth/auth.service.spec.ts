ff58b6c09ddcb5120deb48b2b7d319c79f6b085e
ist1109512 <joao.s.gomes@tecnico.ulisboa.pt>
2025-04-14 18:54:16 +0100

    Solving Ci-CD for BackEnd

--- backend/src/auth/auth.service.spec.ts
+++ backend/src/auth/auth.service.spec.ts
@@ -1,12 +1,28 @@
+// src/auth/auth.service.spec.ts
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from '../users/users.service';
 
 describe('AuthService', () => {
   let service: AuthService;
 
+  const mockUsersService = {
+    findByEmail: jest.fn(),
+    registerUser: jest.fn(),
+  };
+
+  const mockJwtService = {
+    sign: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        { provide: UserService, useValue: mockUsersService },
+        { provide: JwtService, useValue: mockJwtService },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
--- backend/src/users/users.service.spec.ts
+++ backend/src/users/users.service.spec.ts
@@ -1,12 +1,27 @@
+// src/users/users.service.spec.ts
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './users.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 describe('UsersService', () => {
   let service: UserService;
 
+  const mockUserRepository = {
+    findOne: jest.fn(),
+    save: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockUserRepository, // aqui forneces um mock do repositório
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
