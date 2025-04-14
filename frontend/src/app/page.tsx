b2732d483bf4b53dc0197f544ebb64ac8282acfa
Guilherme Monteiro <guilherme.dias.monteiro@tecnico.ulisboa.pt>
2025-04-14 19:56:45 +0100

    Home Page Aligned

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,6 +3,7 @@
 import React, { useEffect } from 'react';
 import Image from "next/image";
 import { useRouter } from 'next/navigation';
+import './home.css';
 
 const HomePage = () => {
   const router = useRouter();
@@ -11,30 +12,20 @@ const HomePage = () => {
     window.location.href = '/login';
   };
 
-  // Garante que a página volta ao topo ao ser carregada novamente
-  useEffect(() => {
-    window.scrollTo(0, 0);
-    document.body.style.background = 'black';
-
-    return () => {
-      // Reseta ao sair da página, se necessário
-      document.body.style.background = '';
-    };
-  }, []);
 
   return (
-    <div className="bg-black w-full flex max-h-screen flex-col items-center overflow-hidden">
+    <div className="">
       
       {/* Logotipo no canto superior esquerdo */}
-      <div className="absolute top-[0%] left-[0%] flex items-center gap-2">
-        <Image src="/images/logo.png" alt="LockBox Logo" width={100} height={100} />
+      <div className="logo">
+        <Image src="/images/logo.png" alt="LockBox Logo" width={160} height={160} />
       </div>
 
       {/* Conteúdo principal */}
-      <div className="flex flex-col items-center justify-start w-full pt-20 px-4">
+      <div className="container">
         
         {/* Título */}
-        <h1 className="text-3xl font-bold text-white mb-6">LockBox</h1>
+        <h1>LockBox</h1>
 
         {/* Imagem */}
         <Image 
@@ -46,14 +37,12 @@ const HomePage = () => {
         />
 
         {/* Slogan */}
-        <p className="text-lg text-white font-light text-center mb-4">
-          Securely manage your passwords anywhere
-        </p>
+        <p>Securely manage your passwords anywhere</p>
 
         {/* Botão */}
         <button
           onClick={handleEnter}
-          className="w-[300px] px-5 py-[15px] rounded-md cursor-pointer text-white text-[16px] font-semibold flex justify-center items-center bg-gradient-to-br from-[#cc3deb] to-[#5b68e4] hover:shadow-[10px_0_20px_#5b69e45c,-10px_0_20px_#cb3deb6e]"
+          className="home-btn"
         >
           Get Started
         </button>
